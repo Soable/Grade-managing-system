ae9b65a08a049aa3555c0b7896fb0c813fb36ade
benngo072009 <benngo072009@gmail.com>
2026-01-24 14:54:08 +0700

    hs_route

--- routes_hs.py
+++ routes_hs.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for
+from models import Student
 
 hs_bp = Blueprint('hs', __name__)
 
 @hs_bp.route('/student/dashboard')
 def dashboard():
-    return render_template('hs/dashboard.html')
+    if 'user_id' not in session or session.get('role') != 'student':
+        return redirect(url_for('login'))
+    
+    student = Student.query.filter_by(user_id=session['user_id']).first()
+    
+    grade = student.grade  # có thể là None
+
+    return render_template(
+        'hs/dashboard.html',
+        student=student,
+        grade=grade
+    )
